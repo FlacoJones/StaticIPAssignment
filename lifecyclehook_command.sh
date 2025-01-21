e7bce22c4bf140fd2bb9b436156457e197e263c9
FlacoJones <andrew@openq.dev>
2025-01-20 18:27:21 -0600

    adds placeholders for shell command

--- lifecyclehook_command.sh
+++ lifecyclehook_command.sh
@@ -1,8 +1,8 @@
 aws autoscaling put-lifecycle-hook \
 	--lifecycle-hook-name ScaleUp \
-	--auto-scaling-group-name \
+	--auto-scaling-group-name {INSERT AUTOSCALING GROUP NAME} \
 	--lifecycle-transition autoscaling:EC2_INSTANCE_LAUNCHING \
-	--notification-target-arn \
-	--role-arn \
+	--notification-target-arn {INSERT SNS TOPIC ARN} \
+	--role-arn {INSERT ROLE ARN FOR AUTOSCALING TO PUBLISH TO SNS} \
 	--heartbeat-timeout 3600 \
 	--default-result ABANDON
